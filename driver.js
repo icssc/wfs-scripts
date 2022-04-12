906f3b03b65c61921af70dc161dd1c1c5b312412
Eddy Chen <89349085+ecxyzzy@users.noreply.github.com>
2022-04-11 21:12:23 -0700

    take advantage of new wfs features in driver

--- driver.js
+++ driver.js
@@ -6,9 +6,29 @@ search.init().then(() => {
     console.timeEnd('Initialization took');
     while (true) {
         try {
-            let q = prompt('Enter a query: ');
+            let query, numResults, mask;
+            while (true) {
+                query = prompt('Enter a query: ');
+                if (!query) {
+                    console.error('Query may not be empty.');
+                } else {
+                    break;
+                }
+            }
+            while (true) {
+                numResults = prompt('Enter the number of results to return [10]: ');
+                if (!numResults) {
+                    numResults = 10;
+                    break;
+                } else if (isNaN(parseInt(numResults))) {
+                    console.error('Must be a number.');
+                } else {
+                    break;
+                }
+            }
+            mask = prompt('(Optional) Enter a comma-separated list of categories to ignore: ');
             console.time('Query took');
-            console.log(search.search(q));
+            console.log(search.search(query, numResults, mask.split(',')));
             console.timeEnd('Query took');
         } catch (e) {
             console.error(e);
