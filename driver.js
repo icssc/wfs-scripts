87fbc10bfc3c61a9d0810c6bba8f828ad5dcfeef
Eddy Chen <89349085+ecxyzzy@users.noreply.github.com>
2022-04-12 13:39:06 -0700

    update driver

--- driver.js
+++ driver.js
@@ -1,37 +1,35 @@
+console.time('Initialization took');
 import promptSync from 'prompt-sync';
 import * as search from 'websoc-fuzzy-search';
 const prompt = promptSync({ sigint: true, eot: true });
-console.time('Initialization took');
-search.init().then(() => {
-    console.timeEnd('Initialization took');
-    while (true) {
-        try {
-            let query, numResults, mask;
-            while (true) {
-                query = prompt('Enter a query: ');
-                if (!query) {
-                    console.error('Query may not be empty.');
-                } else {
-                    break;
-                }
+console.timeEnd('Initialization took');
+while (true) {
+    try {
+        let query, numResults, mask;
+        while (true) {
+            query = prompt('Enter a query: ');
+            if (!query) {
+                console.error('Query may not be empty.');
+            } else {
+                break;
             }
-            while (true) {
-                numResults = prompt('Enter the number of results to return [10]: ');
-                if (!numResults) {
-                    numResults = 10;
-                    break;
-                } else if (isNaN(parseInt(numResults))) {
-                    console.error('Must be a number.');
-                } else {
-                    break;
-                }
-            }
-            mask = prompt('(Optional) Enter a comma-separated list of categories to ignore: ');
-            console.time('Query took');
-            console.log(search.search(query, numResults, mask.split(',')));
-            console.timeEnd('Query took');
-        } catch (e) {
-            console.error(e);
         }
+        while (true) {
+            numResults = prompt('Enter the number of results to return [10]: ');
+            if (!numResults) {
+                numResults = 10;
+                break;
+            } else if (isNaN(parseInt(numResults))) {
+                console.error('Must be a number.');
+            } else {
+                break;
+            }
+        }
+        mask = prompt('(Optional) Enter a comma-separated list of categories to ignore: ');
+        console.time('Query took');
+        console.log(search.search(query, numResults, mask.split(',')));
+        console.timeEnd('Query took');
+    } catch (e) {
+        console.error(e);
     }
-});
+}
